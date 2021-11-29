0c7039e4b456628c43b7e730237d7bffc2ba1eb7
Big B <8c57e820-f3a7-4561-a664-fed7d85bdedc@4wrd.cc>
2021-11-29 11:55:16 -0500

    Updated the manager with the new Item contract - created new item contract to manage individual items

--- contracts/itemmanager.sol
+++ contracts/itemmanager.sol
@@ -1,5 +1,6 @@
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.1;
 
+import './item.sol';
 
 /**
  * The first thing we need is a "Management" Smart Contract, where we can add items
@@ -8,30 +9,34 @@ contract ItemManager {
     //******** DATA STRUCTS ********
     enum SupplyChainState{ Created, Paid, Delivered }
 
-    struct Item {
+    struct SupplyItem {
+        Item _item;
         string _identifier;
         uint _itemPrice;
         ItemManager.SupplyChainState _state;
     }
 
     //******** STORAGE VARIABLES ********
-    mapping( uint => Item ) public items;
+    mapping( uint => SupplyItem ) public items;
 
     uint itemIndex;
 
     //******** EVENTS ********
-    event SupplyChainStep( uint _itemIndex, uint _step );
+    event SupplyChainStep( uint _itemIndex, uint _step, address _itemAddress );
 
     //******** MODIFIERS ********
 
 
     //******** FUNCTIONS ********
     function createItem( string memory _identifier, uint _itemPrice ) public {
+        Item item = new Item(this, _itemPrice, itemIndex);
+
+        items[itemIndex]._item = item;
         items[itemIndex]._identifier = _identifier;
         items[itemIndex]._itemPrice = _itemPrice;
         items[itemIndex]._state = SupplyChainState.Created;
 
-        emit SupplyChainStep( itemIndex, uint(items[itemIndex]._state) );
+        emit SupplyChainStep( itemIndex, uint(items[itemIndex]._state), address(item) );
 
         itemIndex++;
     }
@@ -42,7 +47,7 @@ contract ItemManager {
         
         items[_itemIndex]._state = SupplyChainState.Paid;
 
-        emit SupplyChainStep( itemIndex, uint(items[itemIndex]._state) );
+        emit SupplyChainStep( itemIndex, uint(items[itemIndex]._state), address(items[itemIndex]._item) );
     }
 
     function triggerDelivery( uint _itemIndex ) public {
@@ -50,6 +55,6 @@ contract ItemManager {
 
         items[_itemIndex]._state = SupplyChainState.Delivered;
 
-        emit SupplyChainStep( itemIndex, uint(items[itemIndex]._state) );
+        emit SupplyChainStep( itemIndex, uint(items[itemIndex]._state), address(items[itemIndex]._item) );
     }
 }
