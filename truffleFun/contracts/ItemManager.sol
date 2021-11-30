200166fd0a8716d6c410512e06d4a5c6d7a4f1dc
Big B <8c57e820-f3a7-4561-a664-fed7d85bdedc@4wrd.cc>
2021-11-29 19:41:07 -0500

    Moved contracts, updated truffle-config, imported OZ Contracts

---
+++ truffleFun/contracts/ItemManager.sol
@@ -0,0 +1,61 @@
+pragma solidity ^0.8.1;
+
+import "./Ownable.sol";
+import './Item.sol';
+
+/**
+ * The first thing we need is a "Management" Smart Contract, where we can add items
+ */
+contract ItemManager is Ownable {
+    //******** DATA STRUCTS ********
+    enum SupplyChainState{ Created, Paid, Delivered }
+
+    struct SupplyItem {
+        Item _item;
+        string _identifier;
+        uint _itemPrice;
+        ItemManager.SupplyChainState _state;
+    }
+
+    //******** STORAGE VARIABLES ********
+    mapping( uint => SupplyItem ) public items;
+
+    uint itemIndex;
+
+    //******** EVENTS ********
+    event SupplyChainStep( uint _itemIndex, uint _step, address _itemAddress );
+
+    //******** MODIFIERS ********
+
+
+    //******** FUNCTIONS ********
+    function createItem( string memory _identifier, uint _itemPrice ) public onlyOwner {
+        Item item = new Item(this, _itemPrice, itemIndex);
+
+        items[itemIndex]._item = item;
+        items[itemIndex]._identifier = _identifier;
+        items[itemIndex]._itemPrice = _itemPrice;
+        items[itemIndex]._state = SupplyChainState.Created;
+
+        emit SupplyChainStep( itemIndex, uint(items[itemIndex]._state), address(item) );
+
+        itemIndex++;
+    }
+
+    function triggerPayment( uint _itemIndex ) public payable {
+        require( items[_itemIndex]._itemPrice == msg.value, "Fully payments only, fool!" );
+        require( items[_itemIndex]._state == SupplyChainState.Created, "It is further down in the chain, try again" );
+        
+        items[_itemIndex]._state = SupplyChainState.Paid;
+
+        emit SupplyChainStep( itemIndex, uint(items[itemIndex]._state), address(items[itemIndex]._item) );
+    }
+
+    function triggerDelivery( uint _itemIndex ) public onlyOwner {
+        require( items[_itemIndex]._state == SupplyChainState.Paid, "It is further down in the chain, try again" );
+
+        items[_itemIndex]._state = SupplyChainState.Delivered;
+
+        emit SupplyChainStep( itemIndex, uint(items[itemIndex]._state), address(items[itemIndex]._item) );
+    }
+}
--- truffleFun/migrations/2_deploy_contracts.js
+++ truffleFun/migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
-var SimpleStorage = artifacts.require("./SimpleStorage.sol");
+var ItemManager = artifacts.require("./ItemManager.sol");
 
-module.exports = function(deployer) {
-  deployer.deploy(SimpleStorage);
+module.exports = function (deployer) {
+  deployer.deploy(ItemManager);
 };
