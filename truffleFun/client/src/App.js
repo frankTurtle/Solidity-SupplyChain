bc9f05b61876b7c51d2a6439c26311f4607ad5e8
Big B <8c57e820-f3a7-4561-a664-fed7d85bdedc@4wrd.cc>
2021-11-29 21:05:44 -0500

    Rewrote the app.js to use hooks and interact with the truffle blockchain 💪

--- truffleFun/client/src/App.js
+++ truffleFun/client/src/App.js
@@ -1,73 +1,68 @@
-import React, { Component } from "react";
-import SimpleStorageContract from "./contracts/SimpleStorage.json";
+import React, { useState, useEffect, useRef } from "react";
+
 import getWeb3 from "./getWeb3";
+import ItemManagerContract from './contracts/ItemManager.json';
+import Item from "./contracts/Item.json";
 
 import "./App.css";
 
-class App extends Component {
-  state = { storageValue: 0, web3: null, accounts: null, contract: null };
+const App = () => {
+  const [cost, setCost] = useState(0);
+  const [itemName, setItemName] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
 
-  componentDidMount = async () => {
-    try {
-      // Get network provider and web3 instance.
-      const web3 = await getWeb3();
+  // Get network provider and web3 instance.
+  let web3 = useRef(undefined);
 
-      // Use web3 to get the user's accounts.
-      const accounts = await web3.eth.getAccounts();
+  // Use web3 to get the user's accounts.
+  let accounts = useRef(undefined);;
+  let itemManager = useRef(undefined);;
+  let item = useRef(undefined);;
 
-      // Get the contract instance.
-      const networkId = await web3.eth.net.getId();
-      const deployedNetwork = SimpleStorageContract.networks[networkId];
-      const instance = new web3.eth.Contract(
-        SimpleStorageContract.abi,
-        deployedNetwork && deployedNetwork.address,
-      );
+  useEffect(async () => {
+    web3.current = await getWeb3();
+    accounts.current = await web3.current.eth.getAccounts();
 
-      // Set web3, accounts, and contract to the state, and then proceed with an
-      // example of interacting with the contract's methods.
-      this.setState({ web3, accounts, contract: instance }, this.runExample);
-    } catch (error) {
-      // Catch any errors for any of the above operations.
-      alert(
-        `Failed to load web3, accounts, or contract. Check console for details.`,
-      );
-      console.error(error);
-    }
-  };
+    // Get the contract instance.
+    const networkId = await web3.current.eth.net.getId();
 
-  runExample = async () => {
-    const { accounts, contract } = this.state;
-
-    // Stores a given value, 5 by default.
-    await contract.methods.set(5).send({ from: accounts[0] });
-
-    // Get the value from the contract to prove it worked.
-    const response = await contract.methods.get().call();
-
-    // Update state with the result.
-    this.setState({ storageValue: response });
-  };
-
-  render() {
-    if (!this.state.web3) {
-      return <div>Loading Web3, accounts, and contract...</div>;
-    }
-    return (
-      <div className="App">
-        <h1>Good to Go!</h1>
-        <p>Your Truffle Box is installed and ready.</p>
-        <h2>Smart Contract Example</h2>
-        <p>
-          If your contracts compiled and migrated successfully, below will show
-          a stored value of 5 (by default).
-        </p>
-        <p>
-          Try changing the value stored on <strong>line 42</strong> of App.js.
-        </p>
-        <div>The stored value is: {this.state.storageValue}</div>
-      </div>
+    itemManager.current = new web3.current.eth.Contract(
+      ItemManagerContract.abi,
+      ItemManagerContract.networks[networkId] && ItemManagerContract.networks[networkId].address,
     );
+
+    item.current = new web3.current.eth.Contract(
+      Item.abi,
+      Item.networks[networkId] && Item.networks[networkId].address,
+    );
+
+    setIsLoading(false);
+
+  }, []);
+
+  const handleSubmit = async () => {
+    console.log(itemName, cost, itemManager);
+
+    const result = await itemManager.current.methods.createItem(itemName, cost).send({ from: accounts.current[0] });
+
+    console.log(result);
+    alert("Send " + cost + " Wei to " + result.events.SupplyChainStep.returnValues._itemAddress);
   }
+
+  return (
+    isLoading
+      ? <div>Loading Web3, accounts, and contract...</div>
+      :
+      <div className="App">
+        <h1>Simply Payment/Supply Chain Example!</h1>
+        <h2>Items</h2>
+
+        <h2>Add Element</h2>
+        Cost: <input type="text" name="cost" value={cost} onChange={e => setCost(e.target.value)} />
+        Item Name: <input type="text" name="itemName" value={itemName} onChange={e => setItemName(e.target.value)} />
+        <button type="button" onClick={handleSubmit}>Create new Item</button>
+      </div>
+  );
 }
 
 export default App;
